a1a4777b1044a10e994ed322f4abcc7c822f7f75
Johannes Laire <johannes@laire.fi>
2012-03-28 04:18:23 +0300

    hexout.pl

---
+++ hexout.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use warnings FATAL => 'all';
+use strict;
+
+my @points = map "P$_", grep /\S/, split /\s+/, `cat points.txt`;
+my @grooves = map [grep /\S/, split /\s+/], split /\n\n/, `cat grooves.txt`;
+
+my @names = map [], 0 .. $#points;
+for my $groove (@grooves) {
+	push @{$names[$_]}, $groove->[$_] for 0 .. @$groove - 1;
+}
+
+my %p = map { $_ => [] } @points;
+for my $p (0 .. $#names) {
+	$p{$_} = $points[$p] for @{$names[$p]};
+}
+
+print scalar(@points) . "\n";
+my %groove_size = map { $_ => 1 } 'a' .. 'w';
+for (map @$_, @grooves) {
+	/(\w)(\d)/ or next;
+	$groove_size{$1} = $2 if $2 > $groove_size{$1};
+}
+for my $c ('a' .. 'w') {
+	my $s = $groove_size{$c};
+	for my $l (2 .. 3) {
+		for (1 .. $s-$l+1) {
+			my %ps = map { $p{$c . $_} => 1 } $_ .. $_+$l-1;
+			print join(" ", map { $ps{$_} // 0 } @points) . "\n";
+		}
+	}
+}
