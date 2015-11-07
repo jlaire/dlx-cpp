f8d40ddc9daef9ec100e807402930c05ce118d58
Johannes Laire <johannes@laire.fi>
2015-11-07 04:22:39 +0100

    Fix a corner-case with sparse input

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
 
   std::unique_ptr<LinkedMatrix> lm;
   if (opt_sparse) {
-    lm = LinkedMatrix::from_sparse_matrix(input_rows, secondary_columns);
+    lm = LinkedMatrix::from_sparse_matrix(input_rows, secondary_columns, width);
   }
   else {
     lm = LinkedMatrix::from_boolean_rows(input_rows, secondary_columns);
